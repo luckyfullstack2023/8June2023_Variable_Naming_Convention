2ec6d2872924f870bc9780d927edf74de6da8171
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-09 01:26:23 +0530

    sir 8june assignment done

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+                     // 🔥 let 🔥 
+
+//Declare the letiable_name(fn) with let keyword;
+// let fn ; 
+// Assign the value with lucky (initialize)
+//    fn = "Lucky";
+// Redeclare the letiable_name(fn) with let keyword;
+// let fn;
+// Reassign the value with lucky(same_name) (initialize)
+
+    // fn = "Lucky";
+    // console.log(fn); //A-> Lucky
+    
+// Reassign the value with Happy(different_name) (initialize)
+
+    // fn = "Khamari";
+    // console.log(fn); //A-> Khamari  
+
+
+
+                                                // 🔥 LET 🔥 
+
+//Declare the variable_name(ln) with let keyword;
+let ln ; 
+// Assign the value with lucky (initialize)
+    ln = "Lucky";
+
+//we cannot  Redeclare the variable_name(ln) with let keyword;
+  //let ln;
+
+//  Reassign the value with lucky(same_name) (initialize)
+    ln = "Lucky";
+    console.log(ln); //A-> Lucky
+// Reassign the value with Happy(different_name) (initialize)
+    ln = "Khamari";
+    console.log(ln); //A-> Khamari  
+
+
+
+                                            
+
+
+    
---
+++ js/script1.js
@@ -0,0 +1,70 @@
+            
+                        //🔥  VARIABLE NAMING RULE  🔥 //
+                        
+//1.🥇alphabet{small letter i.e.(a-z)} 
+
+//  var fname = "lucky"; //valid
+// console.log(fname);  //lucky
+
+//2. Alphabet{capital letter i.e.(A-Z)}
+
+// let Lname = "Khamari"; //valid
+// console.log(Lname);    //Khamari
+
+// 3. _underscore (valid)
+
+// const _hh = "hii";   //valid
+// console.log(_hh);    // hii
+
+// 4. $  (valid)
+
+// var $_1 = 1;      //valid
+// console.log($_1); // 1
+
+//5. digit (0-9);
+
+//let 0_l = "one";     //invalid
+// console.log(0_l);
+// let long;
+// long = 1;
+// console.log(long);
+
+
+// var true;
+
+
+// 6. variable_length (no_limit);
+
+let l1 = 1;         //valid
+console.log(l1);    // A-> 1
+
+const lklkjlklkjjsofijkjlfklksjfoijsklsjfksfjisjfkslfks = 2;     //valid
+console.log(lklkjlklkjjsofijkjlfklksjfoijsklsjfksfjisjfkslfks);    //2
+
+// 7. reserve_keyword (Invalid);
+
+//var true = 'false';
+//console.log(true);  //invalid
+
+
+//8. space (Invalid)
+
+//let full name = "lk";    //invalid
+//console.log(full name);  //invalid
+
+// 9.dot(.) anywhere (1st,2nd,3rd)
+
+// var .fname = "lk";     //invalid
+// console.log(.fname);   //invalid
+
+// let f.name = "lk";     //invalid
+// console.log(.fname);  //invalid
+
+// const fname. = "lk";   //invalid
+// console.log(fname.);   //invalid
+
+
+
+
+
+
